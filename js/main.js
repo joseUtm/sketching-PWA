7706b1aededc85792fe90979be7871ed4b277c50
Jose <joseurueta570@gmail.com>
2021-11-28 01:57:39 -0600

    Primer

---
+++ js/main.js
@@ -0,0 +1,40 @@
+import contactoDB, {guardar} from './funciones.js';
+
+/*  SECCION PARA GUARDAR EL FORMULARIO DE CONTACTO */
+
+let bd=contactoDB("Contacto", {contacto:`++id, email, nombre, tel, descripcion`});
+const email = document.getElementById("email");
+const nombre = document.getElementById("nombre");
+const tel = document.getElementById("tel");
+const descripcion = document.getElementById("descripcion");
+
+const btGuardar=document.getElementById("guardar");
+
+btGuardar.onclick=(evento)=>{
+    console.log("Algo");
+    let flag =guardar(bd.contacto, {
+        email: email.value,
+        nombre: nombre.value,
+        tel: tel.value,
+        descripcion: descripcion.value
+    });
+    if(flag){
+        email.value = "";
+        nombre.value = ""
+        tel.value = "";
+        descripcion.value = "";
+    }
+}
+
+/* FIN SECCION PARA GUARDAR FORMULARIO DE CONTACTO */
+
+const app = document.getElementById('typewriter');
+const  typewriter = new Typewriter(app, {
+    loop: true,
+    delay:75
+});
+
+typewriter
+.typeString('La ciudad de la cantera rosa')
+.pauseFor(200)
+.start();
